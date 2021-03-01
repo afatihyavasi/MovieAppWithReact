fa1567f3f17bd924780fe2d5112c00cfbb4f97cd
afatihyavasi <afatihyavasi@gmail.com>
2021-03-01 21:47:49 +0300

    Use API, state, props exercise done.

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import MovieList from './MovieList';
 import axios from 'axios';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-
+const listID = 7079355;
 
 
 class App extends React.Component {
@@ -16,15 +16,19 @@ class App extends React.Component {
 
 
   async componentDidMount() {
-    const getPopulerURL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
-    const response = await axios.get(getPopulerURL);
-    this.setState({ movies: response.data.results })
+    // const getPopulerURL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    // const response = await axios.get(getPopulerURL);
+    // this.setState({ movies: response.data.results })
+    const getListURL = `https://api.themoviedb.org/3/list/${listID}?api_key=${API_KEY}&language=en-US`;
+    const response = await axios.get(getListURL);
+    console.log(response.data.items)
+    this.setState({movies : response.data.items})
   }
 
   deleteMovie = (movie) => {
     const newMovies = this.state.movies.filter(m => m.id !== movie.id);
     this.setState({ movies: newMovies });
-
+    
 
   };
 
@@ -37,7 +41,10 @@ class App extends React.Component {
   render() {
 
     let filteredMovies = this.state.movies.filter(movie => {
-      return movie.title.toLowerCase().includes(this.state.searchQuery.toLowerCase());
+      return (
+        movie.title ? movie.title.toLowerCase().includes(this.state.searchQuery.toLowerCase()) 
+        : movie.title = movie.name 
+      )
     })
 
     return (
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -14,12 +14,13 @@ const MovieList = (props) => {
                     <div className="col-lg-3" key={movie.id}>
                         <div className="card mb-4 shadow-sm">
                             <img src={`https://www.themoviedb.org/t/p/w220_and_h330_face/${movie.poster_path}`} className="card-img-top" alt='sample-movie' />
-                            <div className="card-body">
+                            <div className="card-body overflow-auto">
                                 <h5 className="card-title">{movie.title}</h5>
-                                <p className="card-text">{movie.overview}</p>
+                                <p className="card-text overflow-auto">{movie.overview}</p>
                                 <div className="d-flex justify-content-between align-items-center">
                                     <button type="button" className="btn btn-md btn-outline-danger" onClick={() => deleteCard(movie)}>Delete</button>
-                                    <h2><span className="badge badge-info">{movie.vote_avarage}</span></h2>
+                                    <h2><span className="badge badge-info">{movie.vote_average}</span></h2>
+                                    
                                 </div>
                             </div>
                         </div>
